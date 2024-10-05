f1337feb82894cad0928c5223ebc4c9fa796faf1
Ruslan <ismail96@yandex.ru>
2024-10-05 21:49:11 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,6 +1,6 @@
-first = int(input('1'))
-second = int(input('2'))
-third = int(input('3'))
+first = int(input('1 '))
+second = int(input('2 '))
+third = int(input('3 '))
 if first == second == third:
     print('3')
 elif first == second or first == third or second == third:
